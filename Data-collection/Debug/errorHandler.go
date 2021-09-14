b9a4ada56b5e588dcaf747f9fa8aa5afeb558fbc
Sindre Eiklid <sindre0830@gmail.com>
2021-09-14 11:41:25 +0200

    Added debug structure

--- Data-collection/Debug/errorHandler.go
+++ Data-collection/Debug/errorHandler.go
@@ -1,1 +1,11 @@
 package debug
+
+// Debug structure stores information about errors.
+//
+// Functionality: Update, Print
+type Debug struct {
+	StatusCode 		 int    `json:"status_code"`
+	Location   		 string `json:"location"`
+	RawError   		 string `json:"raw_error"`
+	PossibleReason   string `json:"possible_reason"`
+}
