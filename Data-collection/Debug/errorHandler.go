dccaecf5868d24241742c577a16155cdaab1dbd9
Sindre Eiklid <sindre0830@gmail.com>
2021-09-15 08:53:38 +0200

    Added Update function to Debug structure

--- Data-collection/Debug/errorHandler.go
+++ Data-collection/Debug/errorHandler.go
@@ -1,5 +1,7 @@
 package debug
 
+import "net/http"
+
 // Debug structure stores information about errors.
 //
 // Functionality: Update, Print
@@ -8,4 +10,17 @@ type Debug struct {
 	Location   		 string `json:"location"`
 	RawError   		 string `json:"raw_error"`
 	PossibleReason   string `json:"possible_reason"`
+}
+
+// Update sets new data in structure.
+func (debug *Debug) Update(status int, loc string, err string, reason string) {
+	debug.StatusCode = status
+	debug.Location = loc
+	debug.RawError = err
+	//update reason if status code shows client error
+	if status == http.StatusBadRequest || status == http.StatusNotFound || status == http.StatusMethodNotAllowed {
+		debug.PossibleReason = reason
+	} else {
+		debug.PossibleReason = "Unknown"
+	}
 }
