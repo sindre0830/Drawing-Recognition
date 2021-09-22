fc05c3e6d7db833161ab9ff8a2f7863f220007dc
Sindre Eiklid <sindre0830@gmail.com>
2021-09-22 20:30:10 +0200

    Added init function in main to get api key at startup

--- Data-collection/main.go
+++ Data-collection/main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	dict "main/Dict"
+)
+
+// init runs once at startup.
+func init() {
+	var err error
+	//read API key and branch if an error occurred
+	dict.GOOGLEIMAGES_KEY, err = dict.GetGoogleImagesKey()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 
 // Main program.
 func main() {
