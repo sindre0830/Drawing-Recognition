654239f9d152594db2414fb8dc4ea17b3340d879
Sindre Eiklid <sindre0830@gmail.com>
2021-09-22 19:18:41 +0200

    Added GetGoogleImagesURL to dictionary

--- Data-collection/Dict/dictionary.go
+++ Data-collection/Dict/dictionary.go
@@ -2,3 +2,8 @@ package dict
 
 /* REST services */
 var GOOGLEIMAGES_URL = "https://serpapi.com/search.json?engine=google"
+var GOOGLEIMAGES_KEY = ""
+
+func GetGoogleImagesURL(searchQuery string) string {
+	return GOOGLEIMAGES_URL + "&q=" + searchQuery + "&google_domain=google.com&hl=en&tbm=isch&api_key=" + GOOGLEIMAGES_KEY
+}
