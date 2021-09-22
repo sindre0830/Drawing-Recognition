db0be0a31daf324b47350f11fb246200174ac043
Sindre Eiklid <sindre0830@gmail.com>
2021-09-22 19:58:02 +0200

    Added GetGoogleImagesKey to dictionary

--- Data-collection/Dict/dictionary.go
+++ Data-collection/Dict/dictionary.go
@@ -1,9 +1,19 @@
 package dict
 
+import "io/ioutil"
+
 /* REST services */
 var GOOGLEIMAGES_URL = "https://serpapi.com/search.json?engine=google"
 var GOOGLEIMAGES_KEY = ""
 
 func GetGoogleImagesURL(searchQuery string) string {
 	return GOOGLEIMAGES_URL + "&q=" + searchQuery + "&google_domain=google.com&hl=en&tbm=isch&api_key=" + GOOGLEIMAGES_KEY
+}
+
+func GetGoogleImagesKey() (string, error) {
+	key, err := ioutil.ReadFile("api-key.txt")
+	if err != nil {
+		return "", err
+	}
+	return string(key), nil
 }
