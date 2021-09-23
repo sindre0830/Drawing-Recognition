c7ea770cd2c504c948fec920149b636ebd456f34
Maren <marengrindal@gmail.com>
2021-09-24 01:02:05 +0200

    Added function that puts data from the Google Images API into a slice

--- Data-collection/Api/Thumbnails/thumbnails.go
+++ Data-collection/Api/Thumbnails/thumbnails.go
@@ -16,52 +16,73 @@ func (thumbnails *Thumbnails) Handler(searchQuery string) {
 	// All spaces needs to be replaced with "+" because of the Google Images API
 	searchQuery = strings.ReplaceAll(searchQuery, " ", "+")
 
-	fmt.Println(searchQuery)
-
-	*thumbnails = []string{"test 1", "test 2", "test 3", "test 4"}
-	/*err := thumbnails.get(searchQuery)
+	// Fill the slice with image thumbnails
+	status, err := thumbnails.get(searchQuery)
 	if err != nil {
-		// Handle error
-	}*/
-
-	// Write result to a file
-	err := thumbnails.WriteToFile(strings.ToLower(strings.ReplaceAll(searchQuery, "+", "_")))
-	if err != nil {
-		var debug debug.Debug
-		debug.Update(
-			http.StatusInternalServerError,
-			"Handler() -> WriteToFile() -> Writing thumbnails to file",
+		var errorMessage debug.Debug
+		errorMessage.Update(
+			status,
+			"Thumbnails.Handler() -> Thumbnails.get() -> Get thumbnails from API",
 			err.Error(),
 			"Unknown",
 		)
-		debug.Print()
+		errorMessage.Print()
+	}
+
+	// Write result to a file
+	status, err = thumbnails.WriteToFile(strings.ToLower(strings.ReplaceAll(searchQuery, "+", "_")))
+	if err != nil {
+		var errorMessage debug.Debug
+		errorMessage.Update(
+			status,
+			"Thumbnails.Handler() -> Thumbnails.WriteToFile() -> Writing thumbnails to file",
+			err.Error(),
+			"Unknown",
+		)
+		errorMessage.Print()
 	}
 }
 
+// get the thumbnails from the GoogleImages struct and add them to the thumbnails slice
+func (thumbnails *Thumbnails) get(searchQuery string) (int, error) {
+	var googleImages GoogleImages
+	status, err := googleImages.get(searchQuery)
 
+	for _, image := range googleImages.ImagesResults {
+		*thumbnails = append(*thumbnails, image.Thumbnail)
+	}
+
+	return status, err
+}
 
 // WriteToFile writes all the image thumbnails to a file
-func (thumbnails Thumbnails) WriteToFile(name string) error {
+func (thumbnails *Thumbnails) WriteToFile(name string) (int, error) {
 	path := filepath.Join("data", name)
 
 	// Create the data directory if it does not already exist
 	if _, err := os.Stat("data"); os.IsNotExist(err) {
-		os.MkdirAll("data", 0700)
+		err = os.MkdirAll("data", 0700)
+		if err != nil {
+			return http.StatusInternalServerError, err
+		}
 	}
 
 	// Create and write to a new file if it does not already exist
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		file, err := os.Create(path)
 		if err != nil {
-			return err
+			return http.StatusInternalServerError, err
 		}
 		defer file.Close()
 
 		// Write every value to the file, each on a new line
-		for _, val := range thumbnails {
-			fmt.Fprintln(file, val)
+		for _, val := range *thumbnails {
+			_, err := fmt.Fprintln(file, val)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
 		}
 	}
 
-	return nil
+	return http.StatusOK, nil
 }
