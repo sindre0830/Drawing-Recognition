1d006b43fb79b1a12fc172e856098dae3c9e181d
Maren <marengrindal@gmail.com>
2021-09-28 11:14:30 +0200

    Added check for if the file already exist

--- Data-collection/Api/Thumbnails/thumbnails.go
+++ Data-collection/Api/Thumbnails/thumbnails.go
@@ -10,13 +10,21 @@ import (
 )
 
 // Handler handles the request
-func (thumbnails *Thumbnails) Handler(searchQuery string) {
+func (thumbnails *Thumbnails) Handler(searchQuery string) string {
 	// Make sure every word in the query is separated by ONE space
 	searchQuery = strings.Join(strings.Fields(searchQuery), " ")
 	// All spaces needs to be replaced with "+" because of the Google Images API
 	searchQuery = strings.ReplaceAll(searchQuery, " ", "+")
 
-	// Fill the slice with image thumbnails
+	fileName := strings.ToLower(strings.ReplaceAll(searchQuery, "+", "_"))
+	path := filepath.Join("data", fileName)
+
+	// Return the filename if the file already exists
+	if _, err := os.Stat(path); err == nil {
+		return fileName
+	}
+
+	// Get thumbnails
 	status, err := thumbnails.get(searchQuery)
 	if err != nil {
 		var errorMessage debug.Debug
@@ -27,10 +35,11 @@ func (thumbnails *Thumbnails) Handler(searchQuery string) {
 			"Unknown",
 		)
 		errorMessage.Print()
+		return ""
 	}
 
 	// Write result to a file
-	status, err = thumbnails.WriteToFile(strings.ToLower(strings.ReplaceAll(searchQuery, "+", "_")))
+	status, err = thumbnails.WriteToFile(path)
 	if err != nil {
 		var errorMessage debug.Debug
 		errorMessage.Update(
@@ -40,7 +49,10 @@ func (thumbnails *Thumbnails) Handler(searchQuery string) {
 			"Unknown",
 		)
 		errorMessage.Print()
+		return ""
 	}
+
+	return fileName
 }
 
 // get the thumbnails from the GoogleImages struct and add them to the thumbnails slice
@@ -56,31 +68,27 @@ func (thumbnails *Thumbnails) get(searchQuery string) (int, error) {
 }
 
 // WriteToFile writes all the image thumbnails to a file
-func (thumbnails *Thumbnails) WriteToFile(name string) (int, error) {
-	path := filepath.Join("data", name)
-
-	// Create the data directory if it does not already exist
-	if _, err := os.Stat("data"); os.IsNotExist(err) {
+func (thumbnails *Thumbnails) WriteToFile(path string) (int, error) {
+	// Create a data folder if it does not already exist
+	if _, err := os.Stat("data"); os.IsNotExist(err)  {
 		err = os.MkdirAll("data", 0700)
 		if err != nil {
 			return http.StatusInternalServerError, err
 		}
 	}
 
-	// Create and write to a new file if it does not already exist
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		file, err := os.Create(path)
+	// Create the file
+	file, err := os.Create(path)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	defer file.Close()
+
+	// Write every value to the file, each on a new line
+	for _, val := range *thumbnails {
+		_, err := fmt.Fprintln(file, val)
 		if err != nil {
 			return http.StatusInternalServerError, err
-		}
-		defer file.Close()
-
-		// Write every value to the file, each on a new line
-		for _, val := range *thumbnails {
-			_, err := fmt.Fprintln(file, val)
-			if err != nil {
-				return http.StatusInternalServerError, err
-			}
 		}
 	}
 
