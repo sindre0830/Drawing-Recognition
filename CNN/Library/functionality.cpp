6df611684f40e433d131c9d95bd0fb5f13411869
sindre0830 <sindre0830@gmail.com>
2021-10-06 15:41:21 +0200

    [#64] Added readFile

--- CNN/Library/functionality.cpp
+++ CNN/Library/functionality.cpp
@@ -1,2 +1,19 @@
 /* library */
 #include "functionality.h"
+#include <fstream>
+
+int readFile(const std::string filename, std::list<std::string>& urls) {
+    //reset list
+    urls.clear();
+    //open file and exit upon error
+    std::ifstream file("../../Data-collection/Data/" + filename);
+    if (!file) {
+        return -1;
+    }
+    //read each line in the file and push it to the list
+    std::string url;
+    while (getline(file, url)) {
+        urls.push_back(url);
+    }
+    return 0;
+}
--- CNN/Src/main.cpp
+++ CNN/Src/main.cpp
@@ -1,7 +1,21 @@
 /* library */
 #include "functionality.h"
-
+#include <iostream>
+/**
+ * @brief Main program.
+ */
 int main () {
-    
+    //read file and exit upon error
+    std::string filename = "apple_drawing.txt";
+    std::list<std::string> urls;
+    int err = readFile(filename, urls);
+    if (err != 0) {
+        std::cout << "Failed to read file '" + filename + "'. Exiting program..." << std::endl;
+        return 0;
+    }
+    //print each element in the list
+    for (auto const &url: urls) {
+        std::cout << "'" + url + "'" << std::endl;
+    }
     return 0;
 }
