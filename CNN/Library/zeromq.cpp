01832f206dc44bf45eb326c82dcdee728e1971ec
rickarl <rickarl@stud.ntnu.no>
2021-10-06 14:51:15 +0200

    [#65] Improved zeromq implementation in CNN - now accepts term, and returns path to generated file.

--- CNN/Library/zeromq.cpp
+++ CNN/Library/zeromq.cpp
@@ -11,14 +11,28 @@
 #define sleep(n)	Sleep(n)
 #endif
 
-void client() {
+/**
+*   getDataset
+*   Function that takes a string for the word we want a dataset for,
+*   passes it to our golang server, and waits for a response.
+*
+*   @param  term     -   The term we want a dataset of
+*   @return          -   String containing path to file with dataset
+**/
+std::string getDataset(std::string term) {
     //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REQ);
     socket.connect ("tcp://localhost:5555");
 
-    zmq::message_t request;
+    zmq::message_t request(&term, term.size());
 
     //  Send request
-    socket.send(zmq::str_buffer("Exec"), zmq::send_flags::none);
+    socket.send(zmq::buffer(term), zmq::send_flags::none);
+
+    // wait for reply
+    zmq::message_t reply;
+    socket.recv (reply, zmq::recv_flags::none);
+
+    return reply.to_string();
 }
--- CNN/Src/main.cpp
+++ CNN/Src/main.cpp
@@ -1,5 +1,14 @@
+#include "../Library/Header/zeromq.h"
+#include <iostream>
 
 int main () {
+
+    // I considered whether to put this in its own function too, but I figured
+    // better to move things later when we know how our main should fit together.
+    std::string datasetpath = getDataset("Lion");
+
+    // test it
+    std::cout << "Received: " << datasetpath << std::endl;
     
     return 0;
 }
