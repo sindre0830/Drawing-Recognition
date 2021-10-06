258b483bbfb71197c1be2914dbb6a75b857c1e82
sindre0830 <sindre0830@gmail.com>
2021-10-06 16:10:32 +0200

    [#64] Changed list to vector

--- CNN/Library/functionality.cpp
+++ CNN/Library/functionality.cpp
@@ -2,7 +2,7 @@
 #include "functionality.h"
 #include <fstream>
 
-int readFile(const std::string filename, std::list<std::string>& urls) {
+int readFile(const std::string filename, std::vector<std::string>& urls) {
     //reset list
     urls.clear();
     //open file and exit upon error
@@ -10,7 +10,7 @@ int readFile(const std::string filename, std::list<std::string>& urls) {
     if (!file) {
         return -1;
     }
-    //read each line in the file and push it to the list
+    //read each line in the file and push it to the vector
     std::string url;
     while (getline(file, url)) {
         urls.push_back(url);
--- CNN/Src/main.cpp
+++ CNN/Src/main.cpp
@@ -7,7 +7,7 @@
 int main () {
     //read file and exit upon error
     std::string filename = "apple_drawing.txt";
-    std::list<std::string> urls;
+    std::vector<std::string> urls;
     int err = readFile(filename, urls);
     if (err != 0) {
         std::cout << "Failed to read file '" + filename + "'. Exiting program..." << std::endl;
