3c7ea36d6e080670f3648b25b423eb0a101c7647
sindre0830 <sindre0830@gmail.com>
2021-10-20 14:36:37 +0200

    [#89] Add downloadDataset()

--- CNN-TF/datasetParser.py
+++ CNN-TF/datasetParser.py
@@ -1,3 +1,8 @@
+# import foreign modules
+import requests
+import os
+
+
 # Parses datasets files to lists of URLs.
 def parseDatasetsFiles(datasets):
     arrDatasetsURL = []
@@ -13,3 +18,14 @@ def parseDatasetFile(dataset):
     file = open("../Data-collection/Data/" + dataset + ".txt", "r")
     arrDatasetURL = file.read().splitlines()
     return arrDatasetURL
+
+
+# Download dataset from list of URLs.
+def downloadDataset(dataset, urls):
+    # create directories if needed
+    path = "Data/" + dataset + "/"
+    os.makedirs(path, exist_ok=True)
+    # iterate through urls and request each image
+    for index, url in enumerate(urls, start=1):
+        req = requests.get(url)
+        open(path + str(index) + ".jpg", "wb").write(req.content)
--- CNN-TF/main.py
+++ CNN-TF/main.py
@@ -1,6 +1,8 @@
-# local imports
+# import local modules
 import datasetParser
 
 datasets = ["apple_drawing", "banana", "pear", "strawberry"]
 
 arrDatasetsURL = datasetParser.parseDatasetsFiles(datasets)
+
+datasetParser.downloadDataset(datasets[0], arrDatasetsURL[0])
