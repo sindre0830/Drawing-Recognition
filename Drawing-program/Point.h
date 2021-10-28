d247f1ca5d86cf4cf2e4eab686354435e5ac790f
Maren <marengrindal@gmail.com>
2021-10-28 19:35:28 +0200

    [#87] Fixed commenting in Point class

--- Drawing-program/Point.h
+++ Drawing-program/Point.h
@@ -7,15 +7,20 @@
 class Point {
 private:
 	float x, y,
-		  size;
+		  size,
+		  r, g, b;
 public:
 	Point() { }
-	Point(float x, float y, float size);
-	~Point();
+	Point(float x, float y, float size, float r = 1.f, float g = 0.f, float b = 0.f);
+	~Point() {}
 
 	float getX() { return x; }
 	float getY() { return y; }
 	float getSize() { return size; }
+
+	float getR() { return r; }
+	float getG() { return g; }
+	float getB() { return b; }
 };
 
 #endif // !__POINT_H
