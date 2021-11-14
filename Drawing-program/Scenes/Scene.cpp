3939fcd08885e4cd0f2a481c60f622893b38d49d
Maren <marengrindal@gmail.com>
2021-11-14 22:45:15 +0100

    [#107] Add base class for scenes

---
+++ Drawing-program/Scenes/Scene.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Scene.cpp
+ * @author Maren Skårestuen Grindal
+ * @version 0.1
+ * @date 2021-11-14
+ *
+ * @copyright Copyright (c) 2021 Sindre Eiklid, Rickard Loland, Maren Skårestuen Grindal
+ */
+
+#include "Scene.h"
+
+Scene::Scene() {
+
+}
+
+Scene::~Scene() {
+
+}
+
---
+++ Drawing-program/Scenes/Scene.h
@@ -0,0 +1,23 @@
+/**
+ * @file Scene.h
+ * @author Maren Skårestuen Grindal
+ * @version 0.1
+ * @date 2021-11-14
+ *
+ * @copyright Copyright (c) 2021 Sindre Eiklid, Rickard Loland, Maren Skårestuen Grindal
+ */
+
+#ifndef SCENES_SCENE_H
+#define SCENES_SCENE_H
+
+/**
+ * Base class for all scenes.
+ */
+class Scene {
+ private:
+ public:
+     Scene();
+     ~Scene();
+};
+
+#endif  // SCENES_SCENE_H
